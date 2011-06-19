8ca1d92397d54d8d530dba799d76a567adf40a9f
Adam Collado <datruth2@gmail.com>
2011-06-18 21:56:50 -0400

    Finished problem set 0

---
+++ pset0/ps0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# Problem Set 0
+# Name: Adam Collado
+# Date: 6/18/2011
+
+fname = raw_input("Please enter your first name: ")
+lname = raw_input("Please enter your last name: ")
+
+print "Your full name is %s %s" % (fname, lname)
+print "Your full name is", fname, lname
+
