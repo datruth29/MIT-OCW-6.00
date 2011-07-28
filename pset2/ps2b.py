514de8fc867ef87990c623835b86387b8aa7d0ec
Adam Collado <datruth2@gmail.com>
2011-07-28 11:21:37 -0400

    Updated the code for Problem Set 2b. It no longer has to go through the whole set to find the largest value.

--- pset2/ps2b.py
+++ pset2/ps2b.py
@@ -11,16 +11,29 @@ c = input("Enter a quantity for your 3rd package: ")
 largest = 0
 limit = 200
 solutions = ()
+counter = 0
 
-for n in range(0, limit+1):
-    for z in range(0, n/c+1):
-      for y in range(0,n/b+1):
-        for x in range(0, n/a+1):
-          if x*a + y*b + z*c == n and n not in solutions:
-            solutions += (n,)
 
-for i in range(0, max(solutions)+1):
-  if i not in solutions: largest = i
+for n in range(1, limit+1):
+
+  # If the number of consecutive numbers that solve the equation, then
+  # ANY numbers afterwards will solve for it as well. As such, counter
+  # keeps track of this. The statement below checks for this, and if
+  # it finds it, it breaks out of the logic as it doesn't need to go
+  # further.
+
+  if counter == min(a,b,c):
+    break
+  for z in range(0, n/c+1):
+    for y in range(0,n/b+1):
+      for x in range(0, n/a+1):
+        if x*a + y*b + z*c == n and n not in solutions:
+          solutions += (n,)
+          if max(solutions)-1 not in solutions:
+            largest = max(solutions)-1
+            counter = 0
+          else:
+            counter += 1
 
 print solutions
 print largest
